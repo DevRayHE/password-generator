071be91017eade8e0742c09f7b5afaa7d00e49b7
DevRayHe <DevRayHe@gmail.com>
2021-09-05 22:21:58 +1000

    added password validation - (at least 1 character types of selection is in the password), reduced password length requirement to 4 characters only, to make it easy to check the password validation is working properly, as maximum 4 character types of selection

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ var generatePassword = function() {
     // non numbers input will be converted to NaN.
     passwordLength = Number(lengthInput);
 
-    if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    if (isNaN(passwordLength) || passwordLength < 4 || passwordLength > 128) {
       // Alert error message due to invalid input or password length invalid.
       window.alert("Invalid input! Enter password length numbers only and between 8 to 128.");
       // Calling the function again if invalid input, until valid input is received.
@@ -62,17 +62,26 @@ var generatePassword = function() {
     hasNumeric = window.confirm("Include numeric characters in the password?");
     hasSpecialChar = window.confirm("Include special characters in the password?");
 
-    var validateCharTypeInput = function() { 
-      // Check and alert error message if none of the character type is selected.
-      if ((hasLowerCase + hasUpperCase + hasNumeric + hasSpecialChar) == 0) {
-        window.confirm("Selection error! Please select at least 1 character types.");
-        return getCharType();
-      } else {
-        return [hasLowerCase, hasUpperCase, hasNumeric, hasSpecialChar];
-      }
+    // var validateCharTypeInput = function() { 
+    //   // Check and alert error message if none of the character type is selected.
+    //   if ((hasLowerCase + hasUpperCase + hasNumeric + hasSpecialChar) == 0) {
+    //     window.confirm("Selection error! Please select at least 1 character types.");
+    //     return getCharType();
+    //   } else {
+    //     return [hasLowerCase, hasUpperCase, hasNumeric, hasSpecialChar];
+    //   }
+    // }
+
+    // Check and alert error message if none of the character type is selected.
+    if ((hasLowerCase + hasUpperCase + hasNumeric + hasSpecialChar) == 0) {
+      window.confirm("Selection error! Please select at least 1 character types.");
+      return getCharType();
+    } else {
+      // Return the validated characters types.
+      return [hasLowerCase, hasUpperCase, hasNumeric, hasSpecialChar];
     }
 
-    return validateCharTypeInput();
+    // return validateCharTypeInput();
   }
 
   // Defines a function to validate at least 1 character type is chosen.
@@ -80,55 +89,169 @@ var generatePassword = function() {
   // Function to generate password, password length and characters type as parameters.
   var genPassword = function(passwordLength = getLengthInput(), charType = getCharType()) {
     
-    // Using string to store character types
-    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    // Array position 0 stores boolean value whether that character Type is selected,
+    // Array poaistion 1 stores actual characters.  
+    var lowerCase = [charType[0], "abcdefghijklmnopqrstuvwxyz"];
+    console.log(lowerCase[0], + "...." + lowerCase[1]);
 
-    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var upperCase = [charType[1], "ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 
-    var numeric = "0123456789";
+    var numeric = [charType[2], "0123456789"];
 
-    var specialChar = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    var specialChar = [charType[3], " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
 
-    var password = "";
     var passwordSelection = "";
     
-    // Define variables to store each characters types selection for clarify.
-    var hasLowerCase = charType[0];
-    var hasUpperCase = charType[1];
-    var hasNumeric = charType[2];
-    var hasSpecialChar = charType[3];
+    console.log("PL: " + passwordLength, "\n charAt: " + charType);
 
     // Concatinate character types to passwordSelection, based on user selection.
-    if (hasLowerCase) {
-      passwordSelection += lowerCase;
+    if (lowerCase[0]) {
+      passwordSelection += lowerCase[1];
     }
 
-    if (hasUpperCase) {
-      passwordSelection += upperCase;
+    if (upperCase[0]) {
+      passwordSelection += upperCase[1];
     }
 
-    if (hasNumeric) {
-      passwordSelection += numeric;
+    if (numeric[0]) {
+      passwordSelection += numeric[1];
     }
 
-    if (hasSpecialChar) {
-      passwordSelection += specialChar;
+    if (specialChar[0]) {
+      passwordSelection += specialChar[1];
     }
 
-    // Loop to generate and concatinate to password string.
-    for (i=0; i<passwordLength; i++) {
-      password += passwordSelection.charAt(Math.floor(Math.random() * passwordSelection.length)); 
+    var concatinatePassword = function () {
+      var pass = "";
+      // Loop to generate and concatinate to password string.
+      for (var i=0; i<passwordLength; i++) {
+        pass += passwordSelection.charAt(Math.floor(Math.random() * passwordSelection.length)); 
+      }
+      return pass;
     }
+
+    var password = concatinatePassword();
+    console.log("p: " + password);
+
+    // Function to check password validation, takes in two parameters.
+    // needToCheck is a boolean value, which is whether the character type has been selected.
+    // charToCheck is the string value of the characters
+    var validatePassword = function (needToCheck, charToCheck) {
+      var passwordIsValid = false;
+      if (needToCheck) {
+        // Loop through each string of password to check whether selected character type is generated at least once.
+        for (var i=0; i<passwordLength; i++) {
+          if (password.includes(charToCheck.charAt(i))) {
+            // console.log(lowerCase.charAt(i))
+            // console.log(i + lowerCase.charAt(i));
+            // Found a selected character type in password, return true and exit this function call.
+            return true;
+          }
+          // else {
+          //   console.log("character not detected! " + i + "  " + "character is...  " + lowerCase.charAt(i));
+          // }
+        }
+        // Checked through password, if the selected character type not included, generate password again.
+        if (passwordIsValid === false) {
+          console.log("false detected");
+          // Generate a new password by calling the concatinatePassword function.
+          password = concatinatePassword();
+          validatePassword(needToCheck, charToCheck);
+        }
+      }
+      else {
+        return true;
+      }
+    }
+
+    var lowerCaseChecked = false;
+    var upperCaseChecked = false;
+    var numericChecked = false;
+    var specialCharChecked = false;
+
+    // Call validate password function until a valid password with at least 1 of selected character types each is generated and validated.
+    while ((lowerCaseChecked && upperCaseChecked && numericChecked && specialCharChecked) !== true) {
+      lowerCaseChecked = validatePassword(lowerCase[0], lowerCase[1]);
+      upperCaseChecked = validatePassword(upperCase[0], upperCase[1]);
+      numericChecked = validatePassword(numeric[0], numeric[1]);
+      specialCharChecked = validatePassword(specialChar[0], specialChar[1]);
+    }
+
+    console.log("Password is " + password);
     return password;
+
+    // if (hasUpperCase) {
+    //   for (var i=0; i<passwordLength; i++) {
+    //     if (password.includes(upperCase.charAt(i))) {
+    //       validPassword = true;
+    //       break;
+    //     }
+    //     else {
+    //       console.log("character not detected! " + i + "  " + "character is...  " + upperCase.charAt(i));
+    //       // password = genPassword(passwordLength, charType);
+    //     }
+    //   }
+    //   if (validPassword === false) {
+    //     console.log("false detected");
+    //     password = concatinatePassword();
+    //   }
+    // }
+
+    // if (hasNumeric) {
+    //   for (var i=0; i<passwordLength; i++) {
+    //     if (password.includes(numeric.charAt(i))) {
+    //       validPassword = true;
+    //       break;
+    //     }
+    //     else {
+    //       console.log("Character not detected! " + i + "  " + "character is...  " + numeric.charAt(i));
+    //       // password = genPassword(passwordLength, charType);
+    //     }
+    //   }
+    //   if (validPassword === false) {
+    //     console.log("false detected");
+    //     password = concatinatePassword();
+    //   }
+    // }
+    
+    // if (hasSpecialChar) {
+    //   for (var i=0; i<passwordLength; i++) {
+    //     if (password.includes(specialChar.charAt(i))) {
+    //       validPassword = true;
+    //       break;
+    //     }
+    //     else {
+    //       console.log("character not detected! " + i + "  " + "Character is " + specialChar.charAt(i));
+    //       // password = genPassword(passwordLength, charType);
+    //     }
+    //   }
+    //   if (validPassword === false) {
+    //     console.log("false detected");
+    //     password = concatinatePassword();
+    //   }
+    // }
+
+    // After checked valid password, return password.
+    // if (validPassword === true) {
+    //   return password;
+    // } // If invalid password then Call the the genPassword function to generate password again.
+    // else {
+    //   console.log("ERRRROR detected!! ");
+    // }
   }
 
   var password = genPassword();
+
+  // var validatePassword = function (password, charType = getCharType()) {
+  //   var testA = ['a','b','c'];
+  // }
+
   // lesson learnt, calling a function without passing a parameters when its expected, the parameters will become undefined!!
   return password;
 }
 
 // Password validation:
-  // 1. check whether the user criteria is met (such as if uppercase is chosen, need to have at least 1 uppercase character in the password)
+  // 1. check whether the user criteria is met (such as if uppercase is chosen, need to have at least 1 uppercase character in the password) 
 
   // 2. other checks? Edge cases to minimize error.
 
